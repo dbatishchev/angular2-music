434b419a12ba9d6b302bbe71a114467e70f34630
dbatishchev <dimitrybatishchev@gmail.com>
2016-09-17 13:26:56 +0300

    add some new models and components stubs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { AlbumComponent } from './components/album/album.component';
+import { ArtistComponent } from './components/artist/artist.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AlbumComponent,
+    ArtistComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
